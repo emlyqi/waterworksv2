098604fbbcdd0e0f7084a8c7a5a9c521e96730f4
ANGELA ZHUANG <aangelazz.44@gmail.com>
2024-11-08 21:06:20 -0500

    Final changes of November 8

--- frontend/src/components/ListGroup.tsx
+++ frontend/src/components/ListGroup.tsx
@@ -1,14 +1,30 @@
 function ListGroup() {
+  const items = ["Bob", "Waterfountain1", "Jerry", "Joe", "John"];
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item active" aria-current="true">
-        An active item
-      </li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>Welcome to Waterworks!</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+
+        <li className="list-group-item active" aria-current="true">
+          An active item
+        </li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
